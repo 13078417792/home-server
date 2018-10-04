27be005d1e29894debcc7bcd967aaeae8a0dd9bb
zhouchijian <397201698@qq.com>
2018-10-04 13:30:32 +0800

    缓存改成memcached

--- application/database.php
+++ application/database.php
@@ -13,11 +13,12 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+//    'hostname'        => '127.0.0.1',
+    'hostname'        => '192.168.51.231',
     // 数据库名
     'database'        => 'home-server',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'vm',
     // 密码
     'password'        => 'root',
     // 端口
