4c9860bd14b852b7fecedbde1e834f941329b916
zhouchijian <397201698@qq.com>
2018-10-04 14:20:51 +0800

    404

---
+++ src/views/not-found.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="not-found">
+
+        <div class="main">
+            <div class="icon">
+                <i class="layui-icon">&#xe69c;</i>
+            </div>
+            <h1 class="tips-code">404</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "not-found",
+        created(){
+
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .not-found{
+        user-select:none;
+        cursor:default;
+        width:100%;
+        height:100%;
+        background-color:#eee;
+
+        .main{
+            position:relative;
+            top:50vh;
+            transform:translateY(-30vh);
+
+            @icon-size:15em;
+            .icon{
+                width:@icon-size;
+                margin:0 auto 1em auto;
+
+                .layui-icon{
+                    font-size:@icon-size;
+                }
+            }
+
+            .tips-code{
+                font-size:6em;
+                margin:0;
+                text-align:center;
+                font-weight:lighter;
+                color:#009688;
+            }
+        }
+    }
+</style>
