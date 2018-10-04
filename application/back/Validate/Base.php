cafbe796f4f67705cc63646223745f8b4d37b0e8
zhouchijian <397201698@qq.com>
2018-10-04 13:33:58 +0800

    验证器基类

--- application/back/Validate/Base.php
+++ application/back/Validate/Base.php
@@ -6,18 +6,58 @@
  * Time: 15:31
  */
 
-namespace app\back\Validate;
+namespace app\back\validate;
 
 
 class Base extends \think\Validate{
 
     protected $messages = [];
+    protected $dbName = '';
+    protected $pk = 'id';
+    protected $dbInstance = [];
+
+    public function __construct(array $rules = [], $message = [], $field = []){
+        parent::__construct($rules,$message,$field);
+        if(!$this->dbName){
+            $this->dbName = strtolower(basename(get_class($this)));
+        }
+    }
 
     public function check($data, $rules = [], $scene = '')
     {
-        if($scene && !empty($this->messagrs) && array_key_exists($scene,$this->messages)){
-            $this->message = $this->messages[$scene];
+        if($scene && !empty($this->messages) && array_key_exists($scene,$this->messages)){
+            $this->message = array_merge($this->messages[$scene],$this->message);
         }
         return parent::check($data,$rules,$scene);
+    }
+
+    protected function notNullValidate($value,$data){
+        return $value?true:false;
+    }
+
+    protected function getDb($id,$dbName=null){
+        if(!$dbName){
+            $dbName = $this->dbName;
+        }
+        if(empty($this->dbInstance[$dbName][$id])){
+            $model = ('\app\back\model\\'.ucfirst($dbName))::get($id);
+            $this->dbInstance[$dbName][$id] = $model;
+        }
+        return $this->dbInstance[$dbName][$id];
+    }
+
+    protected function id_exist($value,$rule,$data,$field,$desc){
+//        $model = ('\app\back\model\\'.ucfirst($rule?$rule:$this->dbName))::get($data[$this->pk]);
+        $model = $this->getDb($data[$this->pk]);
+        if(empty($model)){
+            return $desc?:$field.'不存在';
+        }else{
+            return true;
+        }
+    }
+
+    protected function update_unique($value,$rule,$data,$field,$desc){
+        $model = ('\app\back\model\\'.ucfirst($rule?$rule:$this->dbName))::get([$this->pk=>['neq',$data[$this->pk]],$field=>$value]);
+        return empty($model)?true:($desc?:$field).'已存在';
     }
 }
