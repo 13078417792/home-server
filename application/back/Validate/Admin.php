5ac76e63174669f719adf2663d04f5d7085c8767
zhouchijian <397201698@qq.com>
2018-10-04 13:34:47 +0800

    命名空间改小写

--- application/back/Validate/Admin.php
+++ application/back/Validate/Admin.php
@@ -6,14 +6,30 @@
  * Time: 15:31
  */
 
-namespace app\back\Validate;
-
+namespace app\back\validate;
+use \app\back\model\Admin as AdminModel;
 
 class Admin extends Base{
 
+    protected $model = null;
+
     protected $rule = [
-        'username'=>'require',
-        'pwd'=>'require'
+        'id'=>'require|user_exist',
+        'username'=>'require|length:2,15|unique:admin',
+        'nickname'=>'length:2,25|undate_unique',
+        'pwd'=>'require|length:5,25',
+        'role'=>'require|role_exist'
+    ];
+
+    protected $message = [
+        'id.require'=>'用户不存在',
+        'username.require'=>'请输入用户名',
+        'username.length'=>'用户名长度2-15位',
+        'nickname.length'=>'昵称长度2-25位',
+        'pwd.length'=>'密码长度5-25位',
+        'pwd.require'=>'请输入密码',
+        'role.require'=>'请选择角色',
+        'username.unique'=>'用户名已被使用'
     ];
 
     protected $messages = [
@@ -25,15 +41,27 @@ class Admin extends Base{
 
     protected $field = [
         'username'=>'用户名',
-        'pwd'=>'密码'
+        'pwd'=>'密码',
+        'nickname'=>'昵称',
+        'role'=>'角色'
     ];
 
     protected $scene = [
-        'login'=>['username','pwd'=>'require|authUser']
+        'login'=>['username'=>'require','pwd'=>'require|authUser'],
+        'add'=>['username','pwd'=>'length:5,25','role'],
+        'update'=>['id','username'=>'require|length:2,15|undate_unique','nickname','pwd'=>'length','role']
     ];
 
+    protected function getModel($id){
+        if(empty($this->model)){
+            $this->model = AdminModel::get($id);
+        }
+        return $this->model;
+    }
+
     protected function authUser($value,$rule,$data){
-        $detail = db('admin')->where(['username'=>$data['username']])->find();
+//        $detail = db('admin')->where(['username'=>$data['username']])->find();
+        $detail = toArray(AdminModel::get(['username'=>$data['username']]));
         if(empty($detail)){
             return '用户不存在';
         }
@@ -49,6 +77,26 @@ class Admin extends Base{
         }else{
             return '登录失败！未知用户';
         }
+    }
+
+    protected function role_exist($value,$rule,$data){
+        $role_list = db('Role')->where(['status'=>1])->column('id');
+        foreach($value as $v){
+            if(!in_array($v,$role_list)){
+                return '角色不存在';
+            }
+        }
+        return true;
+    }
+
+    protected function user_exist($value,$rule,$data){
+        $model = $this->getModel($data['id']);
+        return empty($model)?'用户不存在':true;
+    }
+
+    protected function undate_unique($value,$rule,$data,$field,$desc){
+        $unique = AdminModel::get(['id'=>['neq',$data['id']],$field=>$data[$field]]);
+        return empty($unique)?true:"{$desc}已被使用";
     }
 
 }
