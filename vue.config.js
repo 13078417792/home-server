30ada1bb6c2bb55fe9cd65cbdd3934c8ab1f4d78
zhouchijian <397201698@qq.com>
2018-10-04 14:17:21 +0800

    1.设置跨域代理服务器 2.设置web-worker的加载器：worker-loader

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+module.exports = {
+    configureWebpack:{
+        devServer:{
+            disableHostCheck: true,
+            proxy: 'http://192.168.51.181:8998/'
+        },
+    },
+    chainWebpack:config=>{
+        config.module.rule('js').exclude.add(/\.worker\.js$/).end()
+        config.module.rule('worker').test(/\.worker\.js$/).use('worker-loader').loader('worker-loader')/*.options({inline:true,fallback:true})*/.end()
+    }
+}
