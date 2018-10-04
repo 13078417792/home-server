c1dc075b67450a8b68e55d7fbb52a34b7f3bcc74
zhouchijian <397201698@qq.com>
2018-10-04 14:20:22 +0800

    首页

---
+++ src/views/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="index">
+        hahahha
+    </div>
+</template>
+
+<script>
+    import framework from '@/layouts/framework'
+    export default {
+        name: "Index",
+        // components:{framework},
+        created(){
+
+        },
+        data(){
+            return {
+
+            };
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
