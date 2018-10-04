cfacd4d5162e685c2393be022345003b7cabb66d
zhouchijian <397201698@qq.com>
2018-10-04 14:37:35 +0800

    视频列表

---
+++ src/views/Video/Video.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="video">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "page-video",
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .video{
+
+    }
+</style>
