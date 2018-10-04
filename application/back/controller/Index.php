c03287a191560bf7a5bd57ce8d2800725e6f2c1f
zhouchijian <397201698@qq.com>
2018-10-04 13:36:41 +0800

    删除测试代码

--- application/back/controller/Index.php
+++ application/back/controller/Index.php
@@ -3,15 +3,5 @@ namespace app\back\controller;
 use \Rbac\Rbac;
 class Index extends \think\Controller{
 
-    public function index(){
-        $r = new Rbac;
-        dd($r->getAdminDetail(1));
-    }
 
-    public function apitest(){
-        if(!request()->isPost()){
-            return json2(false,'请求方式错误');
-        }
-        return json2(true,'成功了',['time'=>$_SERVER['REQUEST_TIME']]);
-    }
 }
