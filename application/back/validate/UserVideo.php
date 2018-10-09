476fa1a9265363ed719374be159b83e7ee8c4efa
zhouchijian <397201698@qq.com>
2018-10-09 22:32:02 +0800

    添加视频信息的验证场景add

--- application/back/validate/UserVideo.php
+++ application/back/validate/UserVideo.php
@@ -23,13 +23,15 @@ class UserVideo extends Base{
         'id.require'=>'视频分类不存在',
         'title.require'=>'标题不能为空',
         'title.length'=>'标题长度为3-100字符',
+        'title.unique'=>'标题已存在',
         'intro.require'=>'简介不能为空',
         'intro.length'=>'简介长度为3-233字符',
         'thumb.require'=>'必须选择或上传一张封面图'
     ];
 
     protected $scene = [
-        'update'=>['id','title','intro','thumb']
+        'update'=>['id','title','intro','thumb'],
+        'add'=>['title'=>'require|length:3,100|unique:user_video','intro','thumb'],
     ];
 
     protected $description = [
