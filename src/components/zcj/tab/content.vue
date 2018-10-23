dbf18ab485a89ff57a0e230a039d55ff9b142d7e
zhouchijian <397201698@qq.com>
2018-10-23 16:30:04 +0800

    tab内容组件

---
+++ src/components/zcj/tab/content.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tab---content" :name="name" v-show="$parent.$props.current===name">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tab-content",
+        props:{
+            label:{
+                type:String,
+                required:true
+            },
+            name:{
+                type:String,
+                required:true
+            },
+            'header-class':{
+                type:String,
+                required:false,
+                default:''
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .tab---content{
+        width:100%;
+        height:100%;
+        overflow-x:hidden;
+        overflow-y:auto;
+    }
+</style>
