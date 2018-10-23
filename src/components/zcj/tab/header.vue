d883dcfdead0ded4f3cea71b93f51ae4c25eb18e
zhouchijian <397201698@qq.com>
2018-10-23 16:30:26 +0800

    tab标题组件

---
+++ src/components/zcj/tab/header.vue
@@ -0,0 +1,63 @@
+<template>
+    <li class="tab-header" :class="{on:$parent.$props.current===name}" @click.stop="toggle">
+        <slot></slot>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "tab-header",
+        props:{
+            name:{
+                type:String,
+                required:true
+            }
+        },
+        data(){
+            return {
+
+            }
+        },
+        methods:{
+            toggle(){
+                this.$emit('toggle',this.$props.name)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+    .tab-header{
+        height:100%;
+        width:100%;
+        list-style:none;
+        font-size:1.3em;
+        text-align:center;
+        cursor:pointer;
+        color:#393D49;
+        transition:color .1s;
+        position:relative;
+        overflow:hidden;
+
+        &:hover{
+            color:#5FB878;
+        }
+
+
+    }
+
+    .tab-header.on{
+
+        @border:3px;
+        &:after{
+            content:'';
+            width:200%;
+            height:@border;
+            background-color:#5FB878;
+            position:absolute;
+            bottom:0;
+            left:0;
+        }
+    }
+</style>
