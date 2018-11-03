26661b7bcc4bf1e6fb2c9f043542a3cb98490256
zhouchijian <397201698@qq.com>
2018-11-03 15:22:00 +0800

    头像组件

---
+++ src/components/zcj/avatar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="zcj-avatar">
+        <img class="avatar-image" v-if="image" :src="image">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "avatar",
+        props:{
+            image:{
+                type:String,
+                default:null
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+    .zcj-avatar{
+        background-color:#e2e2e2;
+        width:100%;
+        height:100%;
+        border-radius:50%;
+        display:inline-block;
+        overflow:hidden;
+
+        .avatar-image{
+            display:block;
+            width:100%;
+            height:100%;
+        }
+    }
+</style>
