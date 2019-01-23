9e6777e8e5b78584a3f4dc3cda8623045f8f09c2
zhouchijian <397201698@qq.com>
2019-01-23 14:41:05 +0800

    获取account模型

--- application/tool/model/Base.php
+++ application/tool/model/Base.php
@@ -8,14 +8,20 @@ use app\tool\model\Account as AccountModel;
 class Base extends Model{
 
     protected $user_id = 0;
+    static $modelInstance = [];
 
     protected function initialize(){
         parent::initialize();
         $this->user_id = $GLOBALS['uid'];
     }
 
-    static public function getAccount(){
-        return AccountModel::find($GLOBALS['uid']);
+    static public function getAccount(int $id=0){
+        if($id<=0) $id = $GLOBALS['uid'];
+        if(!$id) return null;
+        if(empty(self::$modelInstance['Account']) || empty(self::$modelInstance['Account'][$id])){
+            self::$modelInstance['Account'][$id] = AccountModel::find($id);
+        }
+        return self::$modelInstance['Account'][$id];
     }
 
 }
